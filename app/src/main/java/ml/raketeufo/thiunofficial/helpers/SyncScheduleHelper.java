bce1ad11b1bb5591b1ea185f85917bd720eafdd4
Christian Schmied <cs8898@gmx.de>
2020-10-01 13:21:43 +0200

    slight cleanup

--- app/src/main/java/ml/raketeufo/thiunofficial/helpers/SyncScheduleHelper.java
+++ app/src/main/java/ml/raketeufo/thiunofficial/helpers/SyncScheduleHelper.java
@@ -8,23 +8,21 @@ import android.os.Bundle;
 
 import java.util.List;
 
-import ml.raketeufo.thiunofficial.MainActivity;
-import ml.raketeufo.thiunofficial.sync.CalendarSyncLogic;
-
+import ml.raketeufo.thiunofficial.R;
 
 public class SyncScheduleHelper {
-    public static final long SYNC_INTERVAL = 60L * 60L * 2L; //Sync every 2 Houres
-    public static final String AUTHORITY = "ml.raketeufo.thiunofficial.stubprovider";
+    public static final long SYNC_INTERVAL = 60L * 60L * 2L; //Sync every 2 Hours
 
     public static void schedule(Context context) {
         if (AccountManagerHelper.get(context).hasAccount()) {
             Account mAccount = AccountManagerHelper.get(context).getAccount();
+            String authority = context.getString(R.string.stub_provider_authorities);
             ContentResolver mResolver = context.getContentResolver();
-            List<PeriodicSync> addedSyncs = ContentResolver.getPeriodicSyncs(mAccount, AUTHORITY);
+            List<PeriodicSync> addedSyncs = ContentResolver.getPeriodicSyncs(mAccount, authority);
             if (addedSyncs.isEmpty()) {
                 ContentResolver.addPeriodicSync(
                         mAccount,
-                        AUTHORITY,
+                        authority,
                         Bundle.EMPTY,
                         SYNC_INTERVAL);
             }
@@ -34,9 +32,10 @@ public class SyncScheduleHelper {
     public static void sync(Context context) {
         if (AccountManagerHelper.get(context).hasAccount()) {
             Account mAccount = AccountManagerHelper.get(context).getAccount();
+            String authority = context.getString(R.string.stub_provider_authorities);
             ContentResolver mResolver = context.getContentResolver();
             ContentResolver.requestSync(mAccount,
-                    AUTHORITY,
+                    authority,
                     Bundle.EMPTY);
         }
     }
